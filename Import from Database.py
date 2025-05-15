aaa0e7ace96e01cd2fc8188d6cf2bfb733448715
Randal Scott King <scott@randalscottking.com>
2025-05-15 17:30:27 -0400

    Changed credentials

--- Import from Database.py
+++ Import from Database.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://postgres:123Gucc1@localhost/pmry')
+engine = create_engine('postgresql://postgres:1password@localhost/pmry')
 
 df = pd.read_sql_query(' SELECT * FROM "2013" LIMIT 10', engine)
 
